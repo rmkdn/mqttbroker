1a7447215736886127f69a051e26a41ec5b2d2a3
Manoj kumar <61312222+rmkdn@users.noreply.github.com>
2020-04-13 16:00:06 +0530

    Update travis-install.sh

--- travis-install.sh
+++ travis-install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-//if [ "$TRAVIS_OS_NAME" == "linux" ]; then//
-
-if [ "$TRAVIS_OS_NAME" == "heroku" ]; then
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
 	sudo apt-get update -qq
 	sudo apt-get install -y debhelper libc-ares-dev libssl-dev libwrap0-dev python-all python3-all uthash-dev xsltproc docbook-xsl libcunit1-dev
 fi
